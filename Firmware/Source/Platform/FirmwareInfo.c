9ffaa591d69f749064b9c57dcf98cdee2e2d26dc
asytyi <alexander.a.sytyi@gmail.com>
2024-06-14 13:54:40 +0300

    Добавлено сохранение названия проекта в информацию о прошивке

--- Firmware/Source/Platform/FirmwareInfo.c
+++ Firmware/Source/Platform/FirmwareInfo.c
@@ -1,44 +1,48 @@
 // Header
 #include "FirmwareInfo.h"
 
-// Definitions
-#define COMMIT_LEN		7
-#define DATE_LEN		19
-
 // Includes
 #include "git_info.h"
+#include <string.h>
 
 // Forward functions
 void FWINF_AppendSymbol(pInt16U DataTable, Int16U Symbol, Int16U Counter);
+void FWINF_AppendString(pInt16U DataTable, const char *String, Int16U StringLen, pInt16U pCounter, Int16U MaxLength, bool IsLastString);
 
 // Functions
 //
 Int16U FWINF_Compose(pInt16U DataTable, Int16U MaxLength)
 {
-	Int16U BranchLen, i, counter = 0;
-	
-	// Compose commit info
-	for(i = 0; i < COMMIT_LEN && counter < MaxLength; i++)
-		FWINF_AppendSymbol(DataTable, git_commit[i], counter++);
-	if(counter < MaxLength)
-		FWINF_AppendSymbol(DataTable, ',', counter++);
-	
-	// Compose date info
-	for(i = 0; i < DATE_LEN && counter < MaxLength; i++)
-		FWINF_AppendSymbol(DataTable, git_date[i], counter++);
-	if(counter < MaxLength)
-		FWINF_AppendSymbol(DataTable, ',', counter++);
-	
-	// Compose date info
-	BranchLen = sizeof(git_branch) / sizeof(char) - 1;
-	for(i = 0; i < BranchLen && counter < MaxLength; i++)
-		FWINF_AppendSymbol(DataTable, git_branch[i], counter++);
-	
-	// Align counter to even
-	if(counter % 2 && counter < MaxLength)
-		FWINF_AppendSymbol(DataTable, ' ', counter++);
+	Int16U counter = 0;
+
+	FWINF_AppendString(DataTable, git_commit,	strlen(git_commit),	&counter, MaxLength, false);
+	FWINF_AppendString(DataTable, git_date,		strlen(git_date),	&counter, MaxLength, false);
+	FWINF_AppendString(DataTable, git_branch,	strlen(git_branch),	&counter, MaxLength, false);
+	FWINF_AppendString(DataTable, git_proj,		strlen(git_proj),	&counter, MaxLength, true);
 
 	return counter;
+}
+// ----------------------------------------
+
+void FWINF_AppendString(pInt16U DataTable, const char *String, Int16U StringLen, pInt16U pCounter, Int16U MaxLength, bool IsLastString)
+{
+	Int16U i;
+
+	// ƒобавление строки
+	for(i = 0; i < StringLen && *pCounter < MaxLength; i++)
+		FWINF_AppendSymbol(DataTable, String[i], (*pCounter)++);
+	if(*pCounter < MaxLength)
+	{
+		// ¬ыравнивание размера данных
+		if(IsLastString)
+		{
+			if(*pCounter % 2)
+				FWINF_AppendSymbol(DataTable, ' ', (*pCounter)++);
+		}
+		// или добавление разделител€-зап€той
+		else
+			FWINF_AppendSymbol(DataTable, ',', (*pCounter)++);
+	}
 }
 // ----------------------------------------
 
